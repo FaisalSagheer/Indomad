b896d69bf1a6540b0088b5cc05a6d372e78890b2
Faisal <faisalsagheerqureshi@gmail.com>
2025-04-02 12:05:04 +0500

    commit

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -27,7 +27,7 @@ export default function Footer() {
     <>
 
 
-      <footer className="relative w-full border-top border-gray-400">
+      <footer className="relative w-full">
         <div className="mx-auto w-full max-w-7xl px-8">
           <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
             <Typography variant="h5" className="mb-6">
@@ -45,7 +45,7 @@ export default function Footer() {
                 deserunt
                 amet sunt </p>
             </Typography>
-            <div className="grid grid-cols-3 justify-between gap-4">
+            <div className="grid grid-cols-3 justify-between gap-4 pt-7">
               {LINKS.map(({ title, items }) => (
                 <ul key={title}>
                   <Typography
@@ -76,7 +76,7 @@ export default function Footer() {
               variant="small"
               className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
             >
-              &copy; {currentYear} <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+              &copy; {currentYear} <a href="/">Webtick</a>. All
               Rights Reserved.
             </Typography>
             <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
